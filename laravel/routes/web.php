bcada169c052a5c017983a14307b7e3b1058f807
Arshad-SIO <arshad.umair.sio1@gmail.com>
2024-10-15 11:31:24 +0200

    Creer des routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Projet E-COMMERCE";
+});
+
+Route::get('/clients/connexion', function () {
+    return "Page de connexion";
+});
+
+Route::get('/clients/deconnexion', function () {
+    return "Page de deconnexion";
+});
+
+Route::get('/clients/panier', function () {
+	return "Page de panier";
+});
+
+Route::get('/produits', function () {
+	return "Page liste des produits";
+});
+
+Route::get('/produits/description', function () {
+	return "Page de la description de produit";
+});
+
+Route::get('/produits/panier', function () {
+	return "Page de panier lorsqu’on choisit un produit";
+});
+
+Route::get('/panier/commande', function () {
+	return "Page de panier quand on décide de passer la commande";
+});
+
+Route::get('/panier/commande/connexion/paiement', function () {
+	return "Page de paiement";
 });
